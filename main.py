caecef0dc0f7e7c431e2f943bb14e6065a3f267a
GaryMartAlvis <gary.martinez.alvis@gmail.com>
2023-11-05 10:17:01 -0400

    Problema resuelto con una funcion que identifica el año bisiesto.

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+"""
+Problema 1372: Años Bisiesto
+Descripción: Los años bisiestos tienen 366 dias y son aquellos que son multiplos de 4 y no terminan con dos ceros o
+aquellos que despues de quitar los dos ceros del final son divisibles por 4 (divisibles por 400). Por ejemplo 1800 ,
+y 1900 no son años bisiestos, sin embargo el año 2000 si lo fue.
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+Entrada: La entrada consiste de numeros naturales en el rango de 1800 y 9999. Salida: En la salida escriba una palabra
+que diga "si " si fue biciesto y "no " si no fue bisiesto, sin comillas.
+"""
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def leap_yerar(year):
+    ''' Función que dado un año devuelve como resultado si el año es bisiesto o no.
+
+    :param year: Numero dado por el usuario el cual respresenta el año.
+    :return: resultado de la identificación del año como bisiesto o no bisiesto.
+    '''
+
+    if year % 4 == 0:
+        return 'Si'
+    return 'No'
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+result = leap_yerar(2020)
+print(leap_yerar.__doc__)
+print(result)
